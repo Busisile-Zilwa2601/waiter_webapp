560b92ac0108761513317c9aad7bf71c800ba93a
Busisile-Zilwa2601 <busisile.box@gmail.com>
2018-09-27 14:58:47 +0200

    added a style

--- public/js/clickFunction.js
+++ public/js/clickFunction.js
@@ -1,6 +1,12 @@
-$('.label-clickable label').click(function(){
-    var href = $(this).find('input').attr('checked');
-    if(href){
-        window.location = href;
+//check the box when the div is clicked and change color
+$('.label-clickable').click(function(){
+    var $this = $(this), $chk = $this.find('input:checkbox'), checked = $chk.is(':checked'); 
+    $chk.prop('checked', !checked);
+    $this.toggleClass('checked', !checked);
+    if($chk.is(':checked')){
+        $this.addClass("backgroundGrey");
+    }else{
+        $this.removeClass("backgroundGrey");
     }
 });
+
